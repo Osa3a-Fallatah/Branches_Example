b345c265528e9e79e3dcec7c9bd22bbc2fa56699
Nora <Nouraalahmadi1@gmail.com>
2022-01-18 15:44:55 +0300

    Done

--- branches/branches/ViewController.swift
+++ branches/branches/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    func printInfo(){
+        print("Hello i'am Nora alahmadi")
+    }
+}
 
 }
 
